7c97655cc6e0468d3774a81963536e76e8502a14
dirkca <meyer.dirk@gmail.com>
2015-11-15 20:14:51 -0700

    initial commit
    Adding a pait of matching Moodle and Mahara themes

---
+++ moodle/eyekea/layout/header.php
@@ -0,0 +1,6 @@
+<div id="top">
+        <div id="logo"><img src="<?php echo $OUTPUT->pix_url('banner', 'theme'); ?>" alt="banner" /></div>
+        <div id="connect">
+            <img src="<?php echo $OUTPUT->pix_url('logo', 'theme'); ?>" alt="logo" style="margin-bottom: 15px;" />
+        </div>
+    </div>
